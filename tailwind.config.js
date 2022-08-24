d433bfedc202d1a58714fb798e5b4e58eda359c0
versatilemage <104298679+versatilemage@users.noreply.github.com>
2022-08-24 23:44:41 +0530

    imported new image files for usage

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
     extend: {
       backgroundImage: theme => ({
         "robot-hand": "url('/Images/robo-hand.png')",
+        "signal": "url('/Images/Conection Icon.png')",
+        "chips": "url('/Images/chips.png')",
+        "paypal": "url('/Images/emblem-Paypal.jpg')",
       }),
       skew: {
         '30': '-30deg',
