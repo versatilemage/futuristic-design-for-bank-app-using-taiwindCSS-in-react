015b80f490ffe68b8c0276504e62a7a8df65a9d5
versatilemage <104298679+versatilemage@users.noreply.github.com>
2022-08-29 22:52:40 +0530

    added new login component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import {Routes, Route} from "react-router-dom";
 import Landingpage from "./components/landingpage";
+import Login from "./components/login";
 
 function App() {
   return (
     <>
       <Routes>
         <Route exact path="/" element={<Landingpage />}></Route>
+        <Route path="/login" element={<Login />}></Route>
       </Routes>
     </>
   );
